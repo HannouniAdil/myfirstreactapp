080f413ad8b9db1d777bd44c387eaf482580de3c
adilhannouni <adilhannouni@gmail.com>
2020-08-16 00:53:32 -0700

    Add Event-Handler

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,21 @@
 import React, {Component} from 'react';
+import '../css/styles.css'
 
 class Header extends Component {
+
+// inputChangeHandler(){
+//     console.log('I got clicked');
+//   };
+inputChangeHandler(event){
+  console.log(event);
+}  
+
   render(){
     return(
       <header>
         <div>
-          <h1>Enter random number</h1>
-          <input type='text'></input>
+          <h1 className='logo' onClick={this.inputChangeHandler}>Enter random number</h1>
+          <input type='text' onChange={this.inputChangeHandler}></input>
         </div>
       </header>
     )
